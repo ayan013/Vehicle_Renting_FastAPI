7005e29551c6ad0e0807db1d4058df1a3cb22e5e
ayan <ayan787@gmail.com>
2025-07-20 23:07:31 +0530

    Created endpoints for adding vehicles

---
+++ endpoints/vehicles.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends
+from models.vehicle_model import Vehicle,VehicleCreate,VehicleRead
+from sqlmodel import Session
+from db.db import get_db
+from db.vehicle_crud import insert_vehicles
+from typing import List
+
+
+router = APIRouter()
+
+@router.post(path="/add_vehicles",response_model=List[VehicleRead])
+async def add_vehicles(vehicles : List[VehicleCreate], db: Session=Depends(get_db)):
+            return insert_vehicles(db, vehicles)
+
+
