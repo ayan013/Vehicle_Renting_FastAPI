5946e6ee1dba853931f84eaad3d252c0f78b2245
ayan <ayan787@gmail.com>
2025-08-03 21:52:14 +0530

    Updated config file for building dynamic DB url

--- core/config.py
+++ core/config.py
@@ -1,11 +1,33 @@
 from pydantic_settings import BaseSettings
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    DB_USERNAME:str = "ayanpostgres"
+    DB_HOST:str = "vehicle-staging-db.postgres.database.azure.com"
+    DB_PORT:str = "5432"
+    DB_NAME:str = "vehicle_db"
+    KEY_VAULT_NAME:str
+    SECRET_NAME: str
 
     class Config:
         env_file=".env"
         case_sensitive=True
+
+    @property
+    def database_password(self) -> str:
+        vault_url=f"https://{self.KEY_VAULT_NAME}.vault.azure.net/"
+        credential = DefaultAzureCredential()
+        client = SecretClient(vault_url=vault_url,credential=credential)
+        secret = client.get_secret(self.SECRET_NAME)
+        return secret.value
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return(
+            f"postgresql://{self.DB_NAME}:{self.database_password}@"
+            f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}?sslmode=require"
+        )
 
 settings=Settings()
